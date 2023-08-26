c799f39e5528dd24df05293afdcecdae6c6826dd
David Kabanda <116374315+MBChB-Dev@users.noreply.github.com>
2023-08-26 19:45:57 +0200

    Write approach to soliving first step

---
+++ script.js
@@ -0,0 +1,10 @@
+//Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
+
+function getComputerChoice() {
+
+//1) Create an array with the three options
+//2) Create a random index from 0 - 2
+//3) Use the random index above to select one of the options in the array 
+//4) Return that option 
+
+}
